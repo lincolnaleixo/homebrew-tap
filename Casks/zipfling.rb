213f3ceb6fc195be4e28a8775c9c198a4b89e765
Lincoln Aleixo <apple@buyfromus.io>
2026-02-26 19:08:34 +0100

    Initialize ZipFling Homebrew tap

---
+++ Casks/zipfling.rb
@@ -0,0 +1,19 @@
+cask "zipfling" do
+  version "1.0.3"
+  sha256 "7c5bfa8d7ed688213e2d067bdf91b4d1b19308d5270f45573633b2e722908bf4"
+
+  url "https://github.com/lincolnaleixo/zipfling-dist/releases/download/v#{version}/ZipFling-v#{version}.zip"
+  name "ZipFling"
+  desc "Menu bar app for uploading files to S3-compatible storage"
+  homepage "https://github.com/lincolnaleixo/zipfling"
+
+  depends_on macos: ">= :sonoma"
+
+  app "ZipFling.app"
+
+  zap trash: [
+    "~/Library/Application Support/com.lincolnaleixo.ZipFling",
+    "~/Library/Preferences/com.lincolnaleixo.ZipFling.plist",
+    "~/Library/Caches/com.lincolnaleixo.ZipFling",
+  ]
+end
---
+++ README.md
@@ -0,0 +1,8 @@
+# Homebrew Tap for ZipFling
+
+Install:
+
+```bash
+brew tap lincolnaleixo/tap https://github.com/lincolnaleixo/homebrew-tap
+brew install --cask lincolnaleixo/tap/zipfling
+```
