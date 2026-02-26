63c19bdd840c831c9333d1e667473793df686649
lincolnaleixo <lincolnmorais@gmail.com>
2026-02-26 20:28:56 +0100

    Update zipfling cask to v1.0.6

--- Casks/zipfling.rb
+++ Casks/zipfling.rb
@@ -1,6 +1,6 @@
 cask "zipfling" do
-  version "1.0.5"
-  sha256 "a800d17b9706cf8d66dd567271d9b6edac30e615e7ab25ccc8c4c4af36b071e8"
+  version "1.0.6"
+  sha256 "2e96e6a8f9bfca83cd72d58fac7e47eef0c1ec4b84f81245891e67aff3215f6a"
 
   url "https://github.com/lincolnaleixo/zipfling-dist/releases/download/v#{version}/ZipFling-v#{version}.zip"
   name "ZipFling"
